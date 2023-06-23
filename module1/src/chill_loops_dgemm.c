20befa7090e24e7123557b1a0417b5fc1c6cd7b7
Amir Mohammad Tavakkoli <tavakkoli.amirmohammad@gmail.com>
2023-06-23 14:02:48 -0600

    Add Chill loop dgemm

---
+++ module1/src/chill_loops_dgemm.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+double get_time() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec + tv.tv_usec * 1e-6;
+}
+
+
+
+// TODO: COPY THIS FUNCTION THROUGH CHILL AND REPLACE THE BODY WITH MODIFIED VERSION
+void matrixMul(double *A, double *B, double *C, int m, int k, int n) {
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            double sum = 0.0f;
+            for (int l = 0; l < k; ++l) {
+                sum += A[i * k + l] * B[l * n + j];
+            }
+            C[i * n + j] = sum;
+        }
+    }
+}
+// END
+
+int main() {
+    int m = 1024;
+    int n = 1024;
+    int k = 1024;
+    int numIterations = 10;
+
+    // Allocate memory for matrices
+    double *A = (double*)malloc(m * k * sizeof(double));
+    double *B = (double*)malloc(k * n * sizeof(double));
+    double *C = (double*)malloc(m * n * sizeof(double));
+
+    // Initialize input matrices
+    for (int i = 0; i < m * k; ++i) {
+        A[i] = i + 1;
+    }
+
+    for (int i = 0; i < k * n; ++i) {
+        B[i] = -i - 1;
+    }
+
+    // Create variables for timing
+    double startTime, endTime;
+    double totalTime = 0.0;
+
+    for (int iter = 0; iter < numIterations; ++iter) {
+        // Clear the result matrix
+        memset(C, 0, m * n * sizeof(double));
+
+        // Start the timer
+        startTime = get_time();
+
+        // Perform matrix multiplication using loops
+        matrixMul(A, B, C, m, k, n);
+
+        // Stop the timer
+        endTime = get_time();
+
+        // Calculate elapsed time
+        double elapsedTime = endTime - startTime;
+        totalTime += elapsedTime;
+    }
+
+    // Calculate average elapsed time
+    double averageTime = totalTime / numIterations;
+
+    // Calculate number of doubleing point operations (FLOPs)
+    double flops = (2.0e-9 * m * n * k) / averageTime;
+
+    // Print the average elapsed time and FLOPs
+    printf("Matrix multiplication using loops: A(%d, %d) * B(%d, %d) = C(%d, %d)\n", m, k, k, n, m, n);
+    printf("Average execution time: %.5f seconds\n", averageTime);
+    printf("Performance in GFLOPs: %.5f GFLOPs\n", flops);
+
+    // Cleanup
+    free(A);
+    free(B);
+    free(C);
+
+    return 0;
+}
